32c398a046a4cef8a83f88735122550b509a4e06
saymedgm <deepak.cse.dtu@gmail.com>
2024-02-27 11:12:56 +0530

    1

--- src/main/java/com/driver/F1Car.java
+++ src/main/java/com/driver/F1Car.java
@@ -1,26 +1,25 @@
 package com.driver;
 
 public class F1Car {
-	private String carName;
+    private String carName;
     private double speed;
     private int racesWon;
 
     public F1Car(String carName, double speed, int racesWon) {
-       // your code goes here
+        this.carName = carName;
+        this.speed = speed;
+        this.racesWon = racesWon;
     }
 
     public String getCarName() {
-    	// your code goes here
         return carName;
     }
 
     public double getSpeed() {
-    	// your code goes here
         return speed;
     }
 
     public int getRacesWon() {
-    	// your code goes here
         return racesWon;
     }
 }
--- src/main/java/com/driver/F1CarCollection.java
+++ src/main/java/com/driver/F1CarCollection.java
@@ -5,15 +5,14 @@ import java.util.Iterator;
 import java.util.List;
 
 public class F1CarCollection implements Iterable<F1Car> {
-	   private List<F1Car> f1Cars = new ArrayList<>();
+	private List<F1Car> f1Cars = new ArrayList<>();
 
-	    public void addCar(F1Car car) {
-	       // your code goes here
-	    }
+	public void addCar(F1Car car) {
+		f1Cars.add(car);
+	}
 
-	    @Override
-	    public Iterator<F1Car> iterator() {
-	    	//your code goes here
-	        return f1Cars.iterator();
-	    }
+	@Override
+	public Iterator<F1Car> iterator() {
+		return f1Cars.iterator();
+	}
 }
